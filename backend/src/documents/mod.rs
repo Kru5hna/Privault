7bcdf342d0c4b3ed43bd741320526d3255672dd7
Krushna Raut <krishraut2103@gmail.com>
2026-06-21 13:25:18 +0530

    feat(backend): export document router handlers

--- backend/src/documents/mod.rs
+++ backend/src/documents/mod.rs
@@ -10,7 +10,12 @@ pub fn router() -> Router<crate::AppState> {
     Router::new()
         .route("/", get(handlers::list_documents))
         .route("/", post(handlers::upload_document))
-        .route("/:id", get(handlers::download_document))
+        .route("/:id", get(handlers::get_document))
         .route("/:id", delete(handlers::delete_document))
         .route("/folder/:folder_id", delete(handlers::delete_folder_documents))
+        .route("/upload-folder", post(handlers::upload_folder))
+        .route("/:id/download", get(handlers::download_document))
+        .route("/:id/thumbnail", get(handlers::download_thumbnail))
+        .route("/:id/thumbnail", post(handlers::upload_thumbnail))
+        .route("/:id/thumbnail", delete(handlers::delete_thumbnail))
 }
