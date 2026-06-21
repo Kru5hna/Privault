ca887a054b034f0dd1e1901a2f8545df904109a2
Krushna Raut <krishraut2103@gmail.com>
2026-06-21 13:25:18 +0530

    feat(backend): add folder relation models

--- backend/src/folders/models.rs
+++ backend/src/folders/models.rs
@@ -10,3 +10,16 @@ pub struct FolderMetadata {
     pub name: String,
     pub created_at: Option<DateTime<Utc>>,
 }
+
+impl<'r> sqlx::FromRow<'r, sqlx::postgres::PgRow> for FolderMetadata {
+    fn from_row(row: &'r sqlx::postgres::PgRow) -> Result<Self, sqlx::Error> {
+        use sqlx::Row;
+        Ok(Self {
+            id: row.try_get("id")?,
+            owner_id: row.try_get("owner_id")?,
+            parent_id: row.try_get("parent_id")?,
+            name: row.try_get("name")?,
+            created_at: row.try_get("created_at")?,
+        })
+    }
+}
